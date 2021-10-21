9e4e28acafa6ee58ba80e74f2462d9ce6c6c8d15
alapatialekhya <alapati1994@gmail.com>
2021-10-21 19:03:29 +0530

    margin added

--- lib/report_page.dart
+++ lib/report_page.dart
@@ -65,6 +65,7 @@ class _ReportPageState extends State<ReportPage> {
     return Card(
         elevation: 8,
         color: Colors.blueGrey,
+        margin: const EdgeInsets.all(20),
         clipBehavior: Clip.antiAlias,
         child:
             Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
