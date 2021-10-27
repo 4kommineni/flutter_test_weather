195e663c302bde757bffdc11b5e4b4007ffd5b3a
alapatialekhya <alapati1994@gmail.com>
2021-10-27 17:50:33 +0530

    deleted unused import

--- lib/report_page.dart
+++ lib/report_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+
 import 'package:flutter_test_weather/details_page.dart';
 import 'package:flutter_test_weather/wether_model.dart';
 import 'package:intl/intl.dart';
